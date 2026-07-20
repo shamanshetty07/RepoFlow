eeee52004e76d2ed89b8b72540e2092aa14cb1dd
shaman shetty <shamanshetty20067@gmail.com>
2026-07-20 20:25:26 +0530

    feat: integrate Prisma with PostgreSQL adapter and implement user signup service

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,8 +4,14 @@ import { signupService } from "./auth.service.js";
 export const signup=async (req:Request,res:Response)=>{
 
     const {name,email,password}=req.body;
+    try{
     const result=await signupService({
         name,email,password
     })
-    return res.status(201).json(result);
+    return res.status(201).json(result);}
+    catch(err){
+        return res.status(409).json({
+            message:"User already exists"
+        })
+    }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,26 @@
+import prisma from "../config/prisma.js"
+import bcrypt from "bcrypt"
 type SignupData={
     name:string,
     email:string,
     password:string
 }
-export const signupService=async (data:SignupData)=>{
-    return{
-        success:true,
-        message:"signup service is working"
+export const signupService=async (data:SignupData)=>{   
+   const userExists= await prisma.user.findUnique({
+        where:{
+            email:data.email
+        }
+    })
+    if(userExists){
+        throw new Error("User already Exists")
     }
+    const hashedPassword= await bcrypt.hash(data.password,10)
+  const user=await  prisma.user.create({
+        data:{
+        name:data.name,
+        email:data.email,
+        password:hashedPassword
+        },
+    })
+    return user
 }
