b9bc4c196676af126f39356e0f1e875ad5893761
shaman shetty <shamanshetty20067@gmail.com>
2026-08-27 17:27:24 +0530

    feat: integrate chonkie for advanced chunking and enhance chunking service
    - Added chonkie package for improved token chunking capabilities.
    - Updated ChunkingService to utilize TokenChunker for processing file contents.
    - Introduced new chunks interface in repository.types.ts to define chunk structure.
    - Enhanced chunking logic to accommodate new chunking methodology.

--- backend/src/indexing/chunking.service.ts
+++ backend/src/indexing/chunking.service.ts
@@ -1,17 +1,43 @@
-import { processedFiles } from "../types/repository.types.js";
+import { processedFiles, chunks} from "../types/repository.types.js";
 import { chunk, Language } from "code-chunk";
+import {TokenChunker} from 'chonkie';
+import { file } from "zod";
+
+
 export class ChunkingService{
     async chunkfiles(files:processedFiles[]){
         // for(const file of files){
             // console.log(`path of the file ${files[0].path} and contents ${files[0].content}`)
-            const chunks= await chunk(files[2].path,files[2].content,files[2].language);
+        const chunker = await TokenChunker.create({
+            chunkSize:500,
+            chunkOverlap:100
+            } );
+        let chunkFiles:chunks[]=[];
+        for(let i=0;i<files.length;i++){
+        const result= await chunker.chunk(files[i].content);
+        console.log(result.chunk)
+        console.log(result.chunk.text);
+    //    for( const chunck of result){
+    //         const newChunk:chunk={
+    //             content:chunck.text,
+    //             language:chunck.files[i].language,
+    //             filePath:files[i].path
+                
+    //         };
+    //         chunk
+    //    }
+        
+        }
+        
 
-            // console.log(chunks);
-            // console.log(chunk);
+        
+
             
-            for (const chunk of chunks){
-                console.log(chunk.contextualizedText)
-            }
+
+            
+         
+
+            
         // }
     }
 }
--- backend/src/types/repository.types.ts
+++ backend/src/types/repository.types.ts
@@ -16,3 +16,27 @@ export type codeChunk={
     symbolType?:string;
 }
 
+export interface chunks{
+    
+    startIndex:number;
+    endIndex:number;
+    tokenCount:number;
+    chunk:object;
+    path:string;
+    language:string;
+    id:string;
+    startLine:number;
+    endLine:number;
+    chunkIndex:number;
+    symbolName?:string;
+    symbolType?:string;
+
+    parentSymbol?:string;
+    metadata?: {
+        isComplete: boolean;
+        hasImports: boolean;
+        hasComments: boolean;
+    };
+
+
+}
